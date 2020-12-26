d02a80b2b144eeb304eb4b0df9c4e4a87338d626
daryar679 <58885122+daryar679@users.noreply.github.com>
2020-12-26 04:09:31 +0300

    add comments

--- widget.cpp
+++ widget.cpp
@@ -7,18 +7,17 @@ Widget::Widget(QWidget *parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
-    scene = new Scene(this);
-    scene -> setSceneRect(-250,-300,500,600);
+    scene = new Scene(this);//создание сцены
+    scene -> setSceneRect(-250,-300,500,600);//создание области видимости сцены
 
-    QGraphicsPixmapItem * pixItem = new QGraphicsPixmapItem(QPixmap(":/Images/Sky.png"));
-    scene->addItem(pixItem);
+    QGraphicsPixmapItem * pixItem = new QGraphicsPixmapItem(QPixmap(":/Images/Sky.png"));//задний фон игры
+    scene->addItem(pixItem);//добавление заднего фона на сцену
 
-    pixItem->setPos(QPointF(0,0)- QPointF(pixItem->boundingRect().width()/2, pixItem->boundingRect().height()/2));
+    pixItem->setPos(QPointF(0,0)- QPointF(pixItem->boundingRect().width()/2, pixItem->boundingRect().height()/2));//установка положения для корректного отображения заднего фона
 
 
     ui->graphicsView->setScene(scene);
-
-    scene -> addBird();
+    scene -> addBird();//добавление "Птицы" на сцену
 }
 
 Widget::~Widget()
